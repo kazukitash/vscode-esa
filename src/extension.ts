44c11bc5b7ae85540033fa88715a63d2a990c018
kazukitash <kazukitash@gmail.com>
2022-11-21 14:12:27 +0900

    :bug: Fix commands registration

--- src/extension.ts
+++ src/extension.ts
@@ -7,11 +7,11 @@ import { configIsValid } from "./helpers/validations";
 export function activate(context: vscode.ExtensionContext) {
   console.log('"vscode-esa" is now active!');
 
-  let open = vscode.commands.registerCommand("esa.open", () => {
+  let open = vscode.commands.registerCommand("extension.esa.open", () => {
     const esa = vscode.workspace.getConfiguration("esa");
     if (configIsValid(esa)) PostIndexView();
   });
-  let update = vscode.commands.registerCommand("esa.update", () => {
+  let update = vscode.commands.registerCommand("extension.esa.update", () => {
     const esa = vscode.workspace.getConfiguration("esa");
     if (configIsValid(esa)) PostUpdateView();
   });
